ae7884ec8628e9984f9537ce4b2913f14dc06bc9
udeshar <udesh.arlekar@gmail.com>
2023-11-18 19:32:52 +0530

    final commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, Alert } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Alert, FlatList } from 'react-native'
 import React, { useState } from 'react'
 import CustomButton from '../components/CustomButton'
 import FONTS from '../utils/FONTS'
@@ -52,27 +52,18 @@ const Home = ({ navigation, route }) => {
                               />
                               <View style={{ paddingHorizontal: 20, paddingTop: 40, justifyContent: "space-between", flex: 1 }} >
                                         <View style={{ flex: 1 }} >
-                                                  <View style={{ flexDirection: "row", justifyContent: "space-around" }} >
-                                                            <View style={{ ...styles.boxes, backgroundColor: returnColor(1) }} >
-                                                                      <Text style={{ fontFamily: FONTS.BOLD, fontSize: 18 }} >1</Text>
-                                                            </View>
-                                                            <View style={{ ...styles.boxes, backgroundColor: returnColor(2) }}>
-                                                                      <Text style={{ fontFamily: FONTS.BOLD, fontSize: 18 }} >2</Text>
-                                                            </View>
-                                                  </View>
-                                                  <View style={{ flexDirection: "row", justifyContent: "space-around" }} >
-                                                            <View style={{ ...styles.boxes, backgroundColor: returnColor(3) }} >
-                                                                      <Text style={{ fontFamily: FONTS.BOLD, fontSize: 18 }} >3</Text>
-                                                            </View>
-                                                            <View style={{ ...styles.boxes, backgroundColor: returnColor(4) }}>
-                                                                      <Text style={{ fontFamily: FONTS.BOLD, fontSize: 18 }} >4</Text>
-                                                            </View>
-                                                  </View>
-                                                  <View style={{ flexDirection: "row", justifyContent: "space-around" }} >
-                                                            <View style={{ ...styles.boxes, backgroundColor: returnColor(5) }} >
-                                                                      <Text style={{ fontFamily: FONTS.BOLD, fontSize: 18 }} >5</Text>
-                                                            </View>
-                                                  </View>
+                                                  <FlatList 
+                                                            data={[1,2,3,4,5]}
+                                                            keyExtractor={(item) => item.toString()}
+                                                            numColumns={2}
+                                                            renderItem={({item}) => (
+                                                                      <View style={{width : '50%'}} >
+                                                                                <View style={{ ...styles.boxes, alignSelf : "center", backgroundColor: returnColor(item) }} >
+                                                                                          <Text style={{ fontFamily: FONTS.BOLD, fontSize: 18 }} >{item}</Text>
+                                                                                </View>
+                                                                      </View>
+                                                            )}
+                                                  />
                                         </View>
                                         <View>
                                                   {
